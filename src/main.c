25082e01b9f67e4ae1a55d7bcdf4ec5113f2dad0
jodavies <jodavies@users.noreply.github.com>
2018-05-13 12:02:05 +0200

    Clean up, move code from main to functions

--- src/main.c
+++ src/main.c
@@ -31,30 +31,79 @@ const char *fragmentShaderSource = "#version 330 core\n"
 	"   FragColor = colour;\n"
 	"}\0";
 
+int setupOpenGL(GLFWwindow **window, const unsigned int xres, const unsigned int yres,
+                unsigned int *vertexShader, unsigned int *fragmentShader,
+                unsigned int *shaderProgram, unsigned int *vao, unsigned int *vbo);
+void updateGLData();
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+void initializeParticlePositions(float* pos, const float volSize);
+void updateParticlePositions(float *pos, float *vel);
 
 int main(void)
 {
 	printf("attractors\n");
 
+	const int xres = 1900;
+	const int yres = 1180;
+	GLFWwindow *window = NULL;
+	unsigned int vertexShader, fragmentShader, shaderProgram, vao, vbo;
+	setupOpenGL(&window, xres, yres, &vertexShader, &fragmentShader, &shaderProgram, &vao, &vbo);
+
+
+	// point positions and velocities
+	float *pos = malloc(NPARTICLES * 3 * sizeof(float));
+	float *vel = malloc(NPARTICLES * 3 * sizeof(float));
+	initializeParticlePositions(pos, 20.0f);
+	updateGLData(&vbo, pos);
+
+
+	while(!glfwWindowShouldClose(window)) {
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, 1);
+		}
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		updateParticlePositions(pos, vel);
+		updateGLData(&vbo, pos);
+
+		glDrawArrays(GL_POINTS, 0, NPARTICLES);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
+
+
+
+int setupOpenGL(GLFWwindow **window, const unsigned int xres, const unsigned int yres,
+                unsigned int *vertexShader, unsigned int *fragmentShader,
+                unsigned int *shaderProgram, unsigned int *vao, unsigned int *vbo)
+{
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	const int xres = 1900;
-	const int yres = 1180;
 	glfwWindowHint(GLFW_SAMPLES, 4);
-	GLFWwindow *window = glfwCreateWindow(xres, yres, "attractors", NULL, NULL);
-	if(window == NULL) {
+
+	*window = glfwCreateWindow(xres, yres, "attractors", NULL, NULL);
+	if(*window == NULL) {
 		fprintf(stderr, "Error in glfwCreateWindow\n");
 		glfwTerminate();
 		return EXIT_FAILURE;
 	}
-	glfwMakeContextCurrent(window);
+
+	glfwMakeContextCurrent(*window);
+
 	// vsync
 	glfwSwapInterval(1);
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetFramebufferSizeCallback(*window, framebufferSizeCallback);
 	glViewport(0, 0, xres, yres);
 
 	if (glewInit() != GLEW_OK) {
@@ -65,97 +114,82 @@ int main(void)
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	*vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(*vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(*vertexShader);
 	int success;
 	char compileLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	glGetShaderiv(*vertexShader, GL_COMPILE_STATUS, &success);
 	if(!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, compileLog);
+		glGetShaderInfoLog(*vertexShader, 512, NULL, compileLog);
 		fprintf(stderr, "Error in vertex shader compilation:\n%s", compileLog);
 	}
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	*fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(*fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(*fragmentShader);
 
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	*shaderProgram = glCreateProgram();
+	glAttachShader(*shaderProgram, *vertexShader);
+	glAttachShader(*shaderProgram, *fragmentShader);
+	glLinkProgram(*shaderProgram);
+	glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
 	if(!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, compileLog);
+		glGetProgramInfoLog(*shaderProgram, 512, NULL, compileLog);
 		fprintf(stderr, "Error in program compilation:\n%s", compileLog);
 	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	glUseProgram(shaderProgram);
+	glDeleteShader(*vertexShader);
+	glDeleteShader(*fragmentShader);
+	glUseProgram(*shaderProgram);
 
-
-	// point positions and velocities
-	float *pos = malloc(NPARTICLES * 3 * sizeof(float));
-	float *vel = malloc(NPARTICLES * 3 * sizeof(float));
-	// random initial positions, in the range (-20,20)
-	for(size_t i = 0; i < NPARTICLES; i++) {
-		pos[3*i+0] = 40.0*(rand()/(float)RAND_MAX-0.5f);
-		pos[3*i+1] = 40.0*(rand()/(float)RAND_MAX-0.5f);
-		pos[3*i+2] = 40.0*(rand()/(float)RAND_MAX-0.5f);
-	}
-
-	unsigned int VBO,VAO;
 	//gen
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, vao);
+	glGenBuffers(1, vbo);
 	// bind
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*NPARTICLES, pos, GL_STREAM_DRAW);
-
+	glBindVertexArray(*vao);
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-
-	while(!glfwWindowShouldClose(window)) {
-		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-			glfwSetWindowShouldClose(window, 1);
-		}
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// update positions
-		for(int i = 0; i < NPARTICLES; i++) {
-			vel[3*i+0] = (SIGMA*(pos[3*i+1]-pos[3*i+0]));
-			vel[3*i+1] = (pos[3*i+0]*(RHO-pos[3*i+2])-pos[3*i+1]);
-			vel[3*i+2] = (pos[3*i+0]*pos[3*i+1]-BETA*pos[3*i+2]);
-			pos[3*i+0] += STEPSIZE*vel[3*i+0];
-			pos[3*i+1] += STEPSIZE*vel[3*i+1];
-			pos[3*i+2] += STEPSIZE*vel[3*i+2];
-		}
-		// copy new data
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*NPARTICLES, pos, GL_STREAM_DRAW);
-
-		glDrawArrays(GL_POINTS, 0, NPARTICLES);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();    
-	}
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glfwTerminate();
 	return EXIT_SUCCESS;
 }
+
+
+
+void updateGLData(unsigned int *vbo, float *pos)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*NPARTICLES, pos, GL_STREAM_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
 
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+
+
+void initializeParticlePositions(float *pos, const float volSize)
+{
+	for(size_t i = 0; i < NPARTICLES; i++) {
+		pos[3*i+0] = 2.0f * volSize * (rand()/(float)RAND_MAX-0.5f);
+		pos[3*i+1] = 2.0f * volSize * (rand()/(float)RAND_MAX-0.5f);
+		pos[3*i+2] = 2.0f * volSize * (rand()/(float)RAND_MAX-0.5f);
+	}
+}
+
+
+
+void updateParticlePositions(float *pos, float *vel) {
+	for(size_t i = 0; i < NPARTICLES; i++) {
+		vel[3*i+0] = (SIGMA*(pos[3*i+1]-pos[3*i+0]));
+		vel[3*i+1] = (pos[3*i+0]*(RHO-pos[3*i+2])-pos[3*i+1]);
+		vel[3*i+2] = (pos[3*i+0]*pos[3*i+1]-BETA*pos[3*i+2]);
+		pos[3*i+0] += STEPSIZE*vel[3*i+0];
+		pos[3*i+1] += STEPSIZE*vel[3*i+1];
+		pos[3*i+2] += STEPSIZE*vel[3*i+2];
+	}
 }
